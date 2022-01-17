359b2ec1c77615a49076d128ed905a7b616c3b72
mccreate <minwoo6951@naver.com>
2022-01-17 12:05:18 +0900

    Awesome Socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,15 @@
 const socket = io();
 
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+form.addEventListener("submit", handleRoomSubmit);
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
